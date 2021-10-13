5474942e4612a2cc46dfc9090348b6f4aa015ae9
Jake Allen <jake@hintsy.co>
2021-10-13 12:36:19 -0700

    Trying out funny pointer for cursor.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Header from './Containers/Header'
 import Nav from './Containers/Nav'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import nounPointer from './assets/noun-pointer.png'
 
 function App () {
+  console.log(nounPointer)
   return (
     <Provider store={store}>
-      <div className='App'>
+      <div style={{ cursor: `url(${nounPointer}) 20 0, auto` }}>
         <Nav />
         <Header />
       </div>
