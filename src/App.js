b3d477ca7f733da0a1d2d075d7cd9b786c575641
Jake Allen <jake@hintsy.co>
2021-10-14 16:51:03 -0700

    Fixed padding issues, put in mobile cover so people aren't confused.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import nounPointer from './assets/noun-pointer.png'
 import Explainer from './Containers/Explainer';
+import styles from './App.module.css'
 
 function App () {
   return (
     <Provider store={store}>
       <div style={{ cursor: `url(${nounPointer}) 20 0, auto` }}>
+        <div className={styles.mobileCover}>
+          Sorry, the drive-thru isn't taking mobile orders yet (we just bought
+          the place like yesterday). Come back soon.
+        </div>
         <Nav />
         <Header />
         <Explainer />
