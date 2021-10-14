ee70f91ac9d7f84e48cc32726a475423fa424628
Jake Allen <jake@hintsy.co>
2021-10-13 18:00:48 -0700

    Added very quick explainer.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './Containers/Nav'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import nounPointer from './assets/noun-pointer.png'
+import Explainer from './Containers/Explainer';
 
 function App () {
   return (
@@ -10,6 +11,7 @@ function App () {
       <div style={{ cursor: `url(${nounPointer}) 20 0, auto` }}>
         <Nav />
         <Header />
+        <Explainer />
       </div>
     </Provider>
   )
---
+++ src/Containers/Explainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Explainer.module.css'
+
+const Explainer = () => {
+  return (
+    <div className={styles.explainer}>
+      <h1 >Can I take your order?</h1>
+      <div>
+        Fast Food Nouns work at the drive-thru and spend all their hard-earned
+        cash on new clothes and bling. If you have a Fast Food Noun, you can connect
+        your wallet to try on new clothes.
+      </div>
+    </div>
+  )
+}
+
+export default Explainer
