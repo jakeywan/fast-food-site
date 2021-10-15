d03dce7a4f30994c1997d787ada7dd39f0fa88c2
Jake Allen <jake@hintsy.co>
2021-10-14 18:44:09 -0700

    Better explainer text.

--- src/Containers/Explainer.js
+++ src/Containers/Explainer.js
@@ -6,9 +6,12 @@ const Explainer = () => {
     <div className={styles.explainer}>
       <h1 >You want fries with that?</h1>
       <div>
-        Fast Food Nouns work at the drive-thru and spend all their hard-earned
-        cash on new clothes and bling. If you have a Fast Food Noun, you can connect
-        your wallet to try on new clothes.
+        Fast Food Nouns is an on-chain NFT project run by the FastFoodDAO. FFNs
+        are rendered entirely on chain, and can be customized by their owners
+        with on chain clothing and accessories. The designs are curated and funded
+        by the DAO. Shout out to the original <a href='https://nouns.wtf'>Nouns</a>,
+        without whom we would not be here serving up delicious french fries and
+        shakes. Join us on <a href='https://discord.gg/c4stArj9'>Discord</a>. 
       </div>
     </div>
   )
