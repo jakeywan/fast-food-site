5e6edc1e29501aaa46284793b8bab04ee44723fc
Jake Allen <jake@hintsy.co>
2021-10-14 18:38:56 -0700

    Better title.

--- src/Containers/Explainer.js
+++ src/Containers/Explainer.js
@@ -4,7 +4,7 @@ import styles from './Explainer.module.css'
 const Explainer = () => {
   return (
     <div className={styles.explainer}>
-      <h1 >Can I take your order?</h1>
+      <h1 >You want fries with that?</h1>
       <div>
         Fast Food Nouns work at the drive-thru and spend all their hard-earned
         cash on new clothes and bling. If you have a Fast Food Noun, you can connect
